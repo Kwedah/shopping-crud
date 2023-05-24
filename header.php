ceb0e60b2c6cc1170692ba8643164fe9227638b9
Mohamed Kwedah <132231161+Kwedah@users.noreply.github.com>
2023-05-24 19:02:27 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,18 @@
+<header class="header">
+
+    <div class="flix">
+
+        <a href="#" class="logo">Mopile Store</a>
+
+        <nav class="navbar">
+            <a href="admin.php">add products</a>
+            <a href="products.php">view products</a>
+        </nav>
+
+        <a href="crat.php" class="cart">cart <span>0</span></a>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+    </div>
+
+</header>
